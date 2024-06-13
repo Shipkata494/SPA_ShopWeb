8612ccce61813445b2de14035d5a79ad298bd751
nanev494 <163521362+nanev494@users.noreply.github.com>
2024-06-13 16:25:27 +0300

    Add project files.

---
+++ SPA_ShopWeb_Data/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SPA_ShopWeb_Data;
+
+public partial class Item
+{
+    public Guid Id { get; set; }
+
+    public byte[]? ImageData { get; set; }
+
+    public string? ContentType { get; set; }
+
+    public decimal Price { get; set; }
+}
---
+++ SPA_ShopWeb_Data/ShopDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SPA_ShopWeb_Data;
+
+public partial class ShopDbContext : DbContext
+{
+    public ShopDbContext()
+    {
+    }
+
+    public ShopDbContext(DbContextOptions<ShopDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Item> Items { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-RNDBM95\\SQLEXPRESS;Database=ShopDb;Trusted_Connection=true;Integrated Security=true;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.ContentType).HasMaxLength(100);
+            entity.Property(e => e.Price).HasColumnType("decimal(8, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
