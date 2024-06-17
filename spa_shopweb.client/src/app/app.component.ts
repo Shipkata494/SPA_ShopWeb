00e7515f878d4657f3b9fd5d5df3aadacbffdacd
nanev494 <163521362+nanev494@users.noreply.github.com>
2024-06-17 14:45:29 +0300

    Add pictures in assets folder then shows them to view

--- spa_shopweb.client/src/app/app.component.ts
+++ spa_shopweb.client/src/app/app.component.ts
@@ -1,37 +1,51 @@
 import { HttpClient } from '@angular/common/http';
-import { Binary } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-
-interface Items {
-  image: Blob;
+import { Observable } from 'rxjs';
+interface Item {
+  id: string;
   price: number;
   contentType: string;
+  imageData: string;
 }
-
+ interface MyData {
+  Value: string[];
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public items: Items[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
+  items: Item[] = [];
+  sth: MyData | undefined;
+  a: string[] = [];
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.returnImages();
     this.getItems();
   }
-
-  getItems() {
-    this.http.get<Items[]>('/items').subscribe(
+  getAssetsList(): Observable<MyData> {
+    return this.http.get<MyData>('assets/assets-list.json');
+  }
+  returnImages(): void {
+    this.getAssetsList().subscribe(
+      (result) => {
+        this.sth = result;
+        this.a = result.Value
+        }
+    )
+  }
+  getItems(): void {
+    this.http.get<Item[]>('/items').subscribe(
       (result) => {
         this.items = result;
+        result.forEach(x => x.id = x.id.toUpperCase())
+        result.forEach(i => this.a.filter(x => i.id == x.split('.')[0]).map(p=>i.imageData = p))
       },
       (error) => {
-        console.error(error);
+        console.error('Error fetching items:', error);
       }
     );
   }
-
-  title = 'spa_shopweb.client';
 }
+
